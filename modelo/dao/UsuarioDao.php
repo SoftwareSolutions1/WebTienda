b710189e0179da2080be98ba225c838d31145ddd
JimenezR <rojimenezme@gmail.com>
2016-06-11 01:26:35 -0500

    Revert "Archivos no necesarios"
    This reverts commit ad46970b83434d10cdfa5e865260c33c3c032ba7.

---
+++ modelo/dao/UsuarioDao.php
@@ -0,0 +1,57 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+include_once '/xampp/htdocs/PhpBiblio/modelo/db/dbConexion.php';
+include_once '/xampp/htdocs/PhpBiblio/modelo/dto/UsuarioDTO.php';
+
+class usuarioDao{
+    public function login ($usuario){
+        $con = new dbConexion;
+        $con->conectar();
+        $query = "SELECT * FROM usuarios WHERE nombre = '$usuario->usuario' AND contrasenia = '$usuario->contrasenia'";
+        $result = mysql_query($query) or die ("Error al guardar la informacion" .mysql_error()." ");
+        $data = mysql_fetch_assoc($result);
+        $usuario = new UsuarioDTO();
+        $usuario->id = $data ['id'];
+        $usuario->usuario = $data['usuario'];
+        $con->cerrar();
+        return id;
+                
+    }
+    
+    public function getUsuario($id_suscriptor) {
+        $con = new dbConnection();
+        $con->conectar();
+        $query = "SELECT id FROM usuarios_suscriptor WHERE id_suscriptor = '$id_suscriptor'";
+        $result = mysql_query($query) or die("Error al guardar la Informacion" . mysql_error() . "");
+        $data = mysql_fetch_assoc($result);
+        $usuario = new UsuarioDTO();
+        $usuario->id_suscriptor = $data['id_suscriptor'];
+        $usuario->usuario = $data['usuario'];
+        $con->cerrar();
+        return $id;
+    }
+    
+    public function insert(){
+        return true;
+    }
+
+
+    public function eliminar() {
+        return true;
+    }
+
+    public function modificar() {
+        return true;
+    }
+
+    public function consultar() {
+        return true;
+    }
+
+}
